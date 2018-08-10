92a6e6ba4ce68e5261040c5fdc7f6ebf52e7634a
Hao Hou <ghost89413@gmail.com>
2018-08-10 10:04:15 -0600

    Add read and write trait to the Pipe object

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -3,17 +3,25 @@ extern crate libc;
 
 use plumber_rs::*;
 use plumber_rs::servlet::{SyncServlet, ServletFuncResult, Bootstrap, Unimplemented, ServletMode};
-use plumber_rs::pipe::{PipeNum, pipe_define, PIPE_INPUT, PIPE_OUTPUT};
+use plumber_rs::pipe::{Pipe, PIPE_INPUT, PIPE_OUTPUT};
+
+use std::io::{Read, Write};
 
 #[allow(dead_code)]
 struct Servlet {
-    input : PipeNum,
-    output: PipeNum
+    input : Pipe,
+    output: Pipe
 }
 
 impl SyncServlet for Servlet {
     fn init(&mut self, _args:&[&str]) -> ServletFuncResult { Ok(()) }
-    fn exec(&mut self) -> ServletFuncResult { Ok(()) }
+    fn exec(&mut self) -> ServletFuncResult 
+    { 
+        let mut _s = String::new();
+        self.input.read_to_string(&mut _s);
+        write!(self.output, "{}", _s);
+        return Ok(());
+    }
     fn cleanup(&mut self) -> ServletFuncResult { Ok(()) }
 }
 
@@ -24,9 +32,9 @@ impl Bootstrap for BootstrapType {
     type AsyncServletType = Unimplemented;
     fn get(_args:&[&str]) -> Result<ServletMode<Unimplemented, Servlet>, ()>
     {
-        if let Some(input) = pipe_define("input", PIPE_INPUT, Some("$T"))
+        if let Some(input) = Pipe::define("input", PIPE_INPUT, Some("$T"))
         {
-            if let Some(output) = pipe_define("output", PIPE_OUTPUT, None)
+            if let Some(output) = Pipe::define("output", PIPE_OUTPUT, None)
             {
                 return Ok(ServletMode::SyncMode(Servlet{
                     input : input,
--- src/pipe.rs
+++ src/pipe.rs
@@ -1,51 +1,121 @@
 // Copyright (C) 2018, Hao Hou
-use libc::c_char;
 use ::plumber_api::{runtime_api_pipe_t, runtime_api_pipe_flags_t};
 use ::API_ADDRESS_TABLE;
-use std::ptr::null;
-use std::ffi::{CString};
 
-pub type PipeNum   = runtime_api_pipe_t;
+use std::ptr::null;
+use std::io::{Read, Write, Result, Error, ErrorKind};
+use std::ffi::{CString};
+use std::os::raw::c_void;
+use libc::c_char;
+
 pub type PipeFlags = runtime_api_pipe_flags_t;
 
-pub const PIPE_INPUT:PipeFlags   = 0;
-pub const PIPE_OUTPUT:PipeFlags  = 0x10000;
-pub const PIPE_PERSIST:PipeFlags = 0x20000;
-pub const PIPE_ASYNC:PipeFlags   = 0x40000;
-pub const PIPE_SHADOW:PipeFlags  = 0x80000;
-pub const PIPE_DISABLED:PipeFlags = 0x100000;
+// TODO: Currently because of the limit of rust-bindgen, all the constant marcos with non-primitive
+//       type is missing in the bind file. So we have do define it manually
+pub const PIPE_INPUT    :PipeFlags   = 0;
+pub const PIPE_OUTPUT   :PipeFlags   = 0x10000;
+pub const PIPE_PERSIST  :PipeFlags   = 0x20000;
+pub const PIPE_ASYNC    :PipeFlags   = 0x40000;
+pub const PIPE_SHADOW   :PipeFlags   = 0x80000;
+pub const PIPE_DISABLED :PipeFlags   = 0x100000;
 
-// TODO: Pipe cntl flags
 
-pub fn pipe_define(name:&str, flags:PipeFlags, type_expr:Option<&str>) -> Option<PipeNum>
+fn get_cstr(s:Option<&str>) -> (*const c_char, Option<CString>)
 {
-    fn get_cstr(s:Option<&str>) -> (*const c_char, Option<CString>)
+    if let Some(string) = s
     {
-        if let Some(string) = s
+        if let Ok(cstring) = CString::new(string) 
         {
-            if let Ok(cstring) = CString::new(string) 
-            {
-                return (cstring.as_ptr(), Some(cstring));
-            }
-        }
-        return (null(), None);
-    }
-
-    let (name_ptr, _name) = get_cstr(Some(name));
-    let (type_ptr, _type) = get_cstr(type_expr);
-
-    if let Some(ref addr_tab) = unsafe{ API_ADDRESS_TABLE }
-    {
-        if let Some(define_func) = addr_tab.define
-        {
-            let result = unsafe { define_func(name_ptr, flags, type_ptr) };
-
-            if result as i32 != -1
-            {
-                return Some(result);
-            }
+            return (cstring.as_ptr(), Some(cstring));
         }
     }
+    return (null(), None);
+}
 
-    return None;
+
+#[allow(dead_code)]
+pub struct Pipe {
+    pipe : runtime_api_pipe_t
+}
+
+impl Pipe {
+    #[allow(dead_code)]
+    pub fn define(name:&str, flags: PipeFlags, type_expr:Option<&str>) -> Option<Pipe>
+    {
+        let (name_ptr, _name) = get_cstr(Some(name));
+        let (type_ptr, _type) = get_cstr(type_expr);
+
+        if let Some(ref addr_tab) = unsafe{ API_ADDRESS_TABLE }
+        {
+            if let Some(define_func) = addr_tab.define
+            {
+                let result = unsafe { define_func(name_ptr, flags, type_ptr) };
+
+                if result as i32 != -1
+                {
+                    return Some(Pipe{pipe : result});
+                }
+            }
+        }
+        return None;
+    }
+}
+
+impl Read for Pipe {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize>
+    {
+        if let Some(ref addr_tab) = unsafe {API_ADDRESS_TABLE} 
+        {
+            if let Some(read_func) = addr_tab.read
+            {
+                let result = unsafe{ read_func(self.pipe, buf.as_mut_ptr() as *mut c_void, buf.len()) };
+
+                if result as isize != -1
+                {
+                    return Ok(result as usize);
+                }
+                return Err(Error::new(ErrorKind::NotFound, "Plumber pipe_read API returns an error"));
+            }
+            else
+            {
+                return Err(Error::new(ErrorKind::Other, "Plumber pipe_read API doesn't defined"));
+            }
+        }
+        else
+        {
+            return Err(Error::new(ErrorKind::Other, "Plumber guest code runtime doesn't fully initailized"));
+        }
+    }
+}
+
+impl Write for Pipe {
+    fn write(&mut self, buf:&[u8]) -> Result<usize>
+    {
+        if let Some(ref addr_tab) = unsafe {API_ADDRESS_TABLE} 
+        {
+            if let Some(write_func) = addr_tab.write
+            {
+                let result = unsafe { write_func(self.pipe, buf.as_ptr() as *mut c_void, buf.len()) };
+
+                if result as isize != -1
+                {
+                    return Ok(result as usize);
+                }
+                return Err(Error::new(ErrorKind::NotFound, "Plumber pipe_write API returns an error"));
+            }
+            else
+            {
+                return Err(Error::new(ErrorKind::Other, "Plumber pipe_write API doesn't defined"));
+            }
+        }
+        else
+        {
+            return Err(Error::new(ErrorKind::Other, "Plumber guest code runtime doesn't fully initailized"));
+        }
+    }
+
+    fn flush(&mut self) -> Result<()>
+    {
+        return Ok(());
+    }
 }
