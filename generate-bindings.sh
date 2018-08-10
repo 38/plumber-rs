7b587f16150b67bf15c168219f7536dfe3697684
Hao Hou <ghost89413@gmail.com>
2018-08-10 08:53:51 -0600

    Get rid of rust-bindgen from build, its slow and dirty

---
+++ generate-bindings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -z "${BINDGEN}" ]
+then
+	BINDGEN=rust-bindgen
+fi
+
+if [ -z "$(/usr/bin/which ${BINDGEN})" ]
+then
+	echo "Cannot find binary rust-bindgen (hint: set environment variable BINDGEN)" >&2
+	exit 1
+fi
+
+bindgen() {
+	header=$1
+	out=$(basename ${header} .h)_binding.rs
+	echo "[BINDGEN] ${out}" >&2
+	shift
+	${BINDGEN} include/${header} -- ${CFLAGS} $@ > $(dirname $0)/generated/${out} || exit 1
+}
+
+bindgen plumber_api.h -I ${PLUMBER_PREFIX}/include/pservlet
